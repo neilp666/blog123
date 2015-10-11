25545f2a3f91b56b3c0c57acc6bf2512a4a0c3da
neil patel <neil@myindianweddingapp.com>
2015-10-11 11:59:36 +0100

    Removed registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # :registerable - removed
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
